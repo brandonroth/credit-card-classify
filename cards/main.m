c33ceae0f8601918924e277782be1b90c3573395
Brandon Roth <brandon@rocketmade.com>
2013-07-04 16:56:46 -0600

    Initial Commit

---
+++ cards/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  cards
+//
+//  Created by Brandon Roth on 7/4/13.
+//  Copyright (c) 2013 Rocketmade. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
