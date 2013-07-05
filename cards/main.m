71550331dd4eadfae14630089509002b24f401df
Brandon Roth <brandon@rocketmade.com>
2013-07-04 23:23:47 -0600

    Workss

--- cards/main.m
+++ cards/main.m
@@ -7,15 +7,28 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Node.h"
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+
+    
+        Node *head = [Node headNode];
+        [head addCardPrefix:@"4" forCardType:@"Visa"];
+        [head addCardPrefix:@"35" forCardType:@"Amex"];
+        [head addCardPrefix:@"37" forCardType:@"Amex"];
+        [head addCardPrefix:@"3528" forCardType:@"JCB"];
+
+        NSMutableArray *card = [[NSMutableArray alloc] init];
+        [card addObject:@"3"];
+        [card addObject:@"5"];
+        [card addObject:@"2"];
         
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        NSSet * set = [head possibleCardsForCardNumber:card];
+
+        NSLog(@"%@",set);
     }
     return 0;
 }
